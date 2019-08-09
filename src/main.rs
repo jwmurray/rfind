7ad4b3a86d4a376721c34fdebaea865d3914af18
jmurray <john@gardenway.org>
2019-08-09 10:32:14 -0600

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,14 @@
 extern crate clap;
 extern crate walkdir;
+extern crate regex;
 
-use clap::App;
-use clap::Arg;
+mod cli;
+// use cli::get_options;
 
 use walkdir::{DirEntry, WalkDir};
 
-
 fn main() {
-    App::new("rfind")
-        .version("1.0")
-        .about("Unix Find Replacement")
-        .author("John M.")
-        .arg(
-            Arg::with_name("regex_pattern")
-                .short("r")
-                .long("regex")
-                .value_name("regex_pattern")
-                .help("regex pattern for the file systen entry name")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("path")
-                .help("Sets the search path")
-                .default_value(".")
-                .required(false)
-                .index(1),
-        )
-        .arg(
-            Arg::with_name("inner_pattern")
-                .help("Specify pattern for file system entry.")
-                .default_value(".*")
-                .required(false)
-                .index(2),
-        )
-        .get_matches();
+    
+    let options = cli::get_options();
+    println!("options: {:?}", options);
 }
